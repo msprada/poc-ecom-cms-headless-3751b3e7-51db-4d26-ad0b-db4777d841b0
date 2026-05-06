a11d2bac1dd21e49ba47881f94c34bdb373b3c6c
Marcel Sprada <msprada@web.de>
2026-05-06 22:23:54 +0200

    - run format for mjs

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,14 +1,14 @@
 /** @type {import("prettier").Config} */
 export default {
-  printWidth: 100,
-  useTabs: true,
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: ["**/*.astro"],
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+	printWidth: 100,
+	useTabs: true,
+	plugins: ["prettier-plugin-astro"],
+	overrides: [
+		{
+			files: ["**/*.astro"],
+			options: {
+				parser: "astro",
+			},
+		},
+	],
 };
